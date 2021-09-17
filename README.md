dfb8818da32a57b4204979fb9f9c15bd34f3d98a
PhaserEditor2D <developers@phasereditor2d.com>
2021-09-16 22:20:52 -0400

    More about the build script.

--- README.md
+++ README.md
@@ -18,7 +18,13 @@ npm install
 
 2. Copy the latest Phaser Editor 2D server to the `server/` folder. The ElectronJS app will execute the `server/PhaserEditor2D` file.
 
-3. Run the application:
+3. Compiles the TypeScript files
+
+```
+npm run ts-build
+```
+
+4. Run the application:
 
 ```
 npm start
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,3 +1,18 @@
-import { startElectron } from "./startElectron";
+import { app, BrowserWindow } from "electron";
+import { WindowManager } from "./windowManager";
 
-startElectron()
+
+app.whenReady().then(() => new WindowManager())
+
+app.on("window-all-closed", () => {
+
+    process.exit()
+})
+
+app.on("activate", () => {
+
+    if (BrowserWindow.getAllWindows().length === 0) {
+
+        new WindowManager()
+    }
+})
