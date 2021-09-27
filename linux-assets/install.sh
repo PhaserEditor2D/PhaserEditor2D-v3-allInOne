6eecb7efdaaae6d910c82f49a07ce8e8dac6f7f0
PhaserEditor2D <developers@phasereditor2d.com>
2021-09-27 11:19:50 -0400

    Sets dev version.

--- linux-assets/install.sh
+++ linux-assets/install.sh
@@ -4,11 +4,11 @@ DIR=$(dirname "$SCRIPT")
 
 echo [Desktop Entry] > phasereditor2d-PhaserEditor2D.desktop
 echo Type=Application >> phasereditor2d-PhaserEditor2D.desktop
-echo Name=Phaser Editor 2D v3 >> phasereditor2d-PhaserEditor2D.desktop
+echo Name=Phaser Editor 2D All In One v3 >> phasereditor2d-PhaserEditor2D.desktop
 echo GenericName=Game IDE >> phasereditor2d-PhaserEditor2D.desktop
 echo Comment=HTML5 game development editor. >> phasereditor2d-PhaserEditor2D.desktop
 echo Terminal=false >> phasereditor2d-PhaserEditor2D.desktop
-echo Exec=$DIR/PhaserEditor2D >> phasereditor2d-PhaserEditor2D.desktop
+echo Exec=$DIR/PhaserEditor2D-allInOne >> phasereditor2d-PhaserEditor2D.desktop
 echo Icon=$DIR/resources/app/linux-assets/icon.png >> phasereditor2d-PhaserEditor2D.desktop
 
 chmod +x ./phasereditor2d-PhaserEditor2D.desktop
