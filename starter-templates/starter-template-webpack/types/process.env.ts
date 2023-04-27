b92fcf8795c5215c144aeade2a694567cd78ae22
PhaserEditor2D <developers@phasereditor2d.com>
2023-04-26 22:14:30 -0400

    Get latest templates

---
+++ starter-templates/starter-template-webpack/types/process.env.ts
@@ -0,0 +1,7 @@
+// define the Node "process" object. If it is defined (@types/node) by another module you installed,
+// then you can delete this file
+declare const process: {
+    env: {
+        NODE_ENV: string
+    }
+}
